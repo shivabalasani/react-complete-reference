4c184f4c8a8147c5b201e2a01c5502eedd21a081
cslplasma <54950381+cslplasma@users.noreply.github.com>
2020-07-10 14:12:05 -0400

    Updated React version to 16

--- react-course-2-expensify-app/src/tests/setupTests.js
+++ react-course-2-expensify-app/src/tests/setupTests.js
@@ -1,1 +1,8 @@
-require('dotenv').config({ path: '.env.test' });
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({
+    adapter: new Adapter()
+});
+
+//require('dotenv').config({ path: '.env.test' });
